60dcb04af63020057a183650e478495032266153
carlosnacamura <127620688+carlosnacamura@users.noreply.github.com>
2024-11-25 08:13:23 -0300

    Substituindo possível repositório ñ bugado

---
+++ SistemaCad_AlCur-main/CadAlunCurs/Formulários/Cadastrar/FrmCadastroAluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadAlunCurs.Formulários.Cadastrar
+{
+    public partial class FrmCadastroAluno : Form
+    {
+        public FrmCadastroAluno()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSenha_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpNascimento_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
